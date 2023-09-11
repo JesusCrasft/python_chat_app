6a58200c50d01df7564205d788c8d256c5e796f4
Jesus <jesus.flores.angulo@gmail.com>
2023-09-11 11:46:18 -0500

    Se agregaron las 3 labels principales

--- index.py
+++ index.py
@@ -14,7 +14,22 @@ class Product:
         self.username = ''
 
         """Labels"""
-        
+        #Label Username
+        self.label_username = Label(self.wind, text='one')
+        self.label_username.configure(background='#1F1F1F', relief=SOLID, borderwidth=2, fg='gray')
+        self.label_username.place(relwidth = 0.10, relheight = 0.09, relx = 0.0, rely = 0.0)
+
+        #Label Contacts
+        self.label_contacts = Label(self.wind, text='two')
+        self.label_contacts.configure(background='#1F1F1F', relief=SOLID, borderwidth=2, fg='gray')
+        self.label_contacts.place(relwidth = 0.10, relheight = 0.09, relx = 0.25, rely = 0.50)
+
+
+        #Label Chat
+        self.label_chat = Label(self.wind, text='three')
+        self.label_chat.configure(background='#1F1F1F', relief=SOLID, borderwidth=2, fg='gray')
+        self.label_chat.place(relwidth = 0.10, relheight = 0.09, relx = 0.70, rely = 0.0)
+
 
 if __name__ == '__main__':
     WindowT = Tk()
