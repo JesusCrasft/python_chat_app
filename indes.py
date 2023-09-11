894cb331ed010c8fb951104d878f307b505ac499
Jesus <jesus.flores.angulo@gmail.com>
2023-09-11 11:08:09 -0500

    Se creo la ventana principal

---
+++ indes.py
@@ -0,0 +1,19 @@
+from tkinter import ttk
+from tkinter import * 
+
+class Product:
+
+    def __init__(self, WindowT):
+        #Window Attributes
+        self.wind = WindowT
+        self.wind.title('Chat')
+        self.wind.geometry('1000x800')
+        self.wind.configure(bg='#1F1F1F')
+
+        #Variables
+
+
+if __name__ == '__main__':
+    WindowT = Tk()
+    application = Product(WindowT)
+    WindowT.mainloop()
