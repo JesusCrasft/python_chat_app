174abc727889174028dd671e7ab5e5cbb34f9dbe
Jesus <jesus.flores.angulo@gmail.com>
2023-09-12 11:55:54 -0500

    Se agrego una label extra para evitar el conflicto con el scrolledframe

--- index.py
+++ index.py
@@ -16,6 +16,8 @@ class App:
         #Variables
         self.my_socket = socket.socket()
         self.username = ''
+        self.x = 0.80
+        self.y = 0
        
 
         """Labels"""
@@ -32,7 +34,12 @@ class App:
         #Chat Label
         self.label_chat = Label(self.wind)
         self.label_chat.configure(background='#1F1F1F', relief=SOLID, borderwidth=2, fg='gray')
-        self.label_chat.place(relwidth = 0.70, relheight = 0.9999, relx = 0.30, rely = 0.0)
+        self.label_chat.place(relwidth = 0.70, relheight = 0.90, relx = 0.30, rely = 0.0)
+
+        #Widget Chat Label
+        self.label_wchat = Label(self.wind)
+        self.label_wchat.configure(background='#1F1F1F', relief=SOLID, borderwidth=2, fg='gray')
+        self.label_wchat.place(relwidth = 0.70, relheight = 0.10, relx = 0.30, rely = 0.90)
 
 
         """Entrys"""
@@ -42,29 +49,29 @@ class App:
         self.entry_contacts.place(relwidth = 0.9999, relheight = 0.05, relx = 0.0, rely = 0.0)
 
         #Chat Entry
-        self.entry_chat = Entry(self.label_chat, font=('Arial', 15))
+        self.entry_chat = Entry(self.label_wchat, font=('Arial', 15))
         self.entry_chat.configure(exportselection=False, bg='#323232', fg='white', highlightbackground='gray')
-        self.entry_chat.place(relwidth = 0.75, relheight = 0.06, relx = 0.02, rely = 0.92)
+        self.entry_chat.place(relwidth = 0.75, relheight = 0.65, relx = 0.02, rely = 0.08)
 
 
         """Buttons"""
         #Chat Button
-        self.button_chat = Button(self.label_chat, text='Enviar', command=self.place_mymessage)
-        self.button_chat.place(relwidth = 0.10, relheight = 0.05, relx = 0.78, rely = 0.92)
+        self.button_chat = Button(self.label_wchat, text='Enviar', command=self.place_mymessage)
+        self.button_chat.place(relwidth = 0.10, relheight = 0.65, relx = 0.78, rely = 0.08)
 
         #Image Button
-        self.button_sendimg = Button(self.label_chat, text='Imagen')
-        self.button_sendimg.place(relwidth = 0.10, relheight = 0.05, relx = 0.89, rely = 0.92)
+        self.button_sendimg = Button(self.label_wchat, text='Imagen')
+        self.button_sendimg.place(relwidth = 0.10, relheight = 0.65, relx = 0.89, rely = 0.08)
+
 
         """ScrolledFrame"""
         #Create a ScrolledFrame widget
         self.frame_helper = ScrolledFrame(self.label_chat)
-        self.frame_helper.place(relwidth = 0.99, relheight = 0.91, relx = 0.0, rely = 0.0)
+        self.frame_helper.pack(side=TOP, fill=BOTH, expand=1)
         self.frame_helper.bind_scroll_wheel(self.wind)
 
         #Create a frame within the ScrolledFrame
         self.frame_chat = self.frame_helper.display_widget(Frame) 
-        self.frame_chat.place(relwidth = 0.99, relheight = 0.91, relx = 0.0, rely = 0.0)
         
 
 
@@ -72,7 +79,9 @@ class App:
         message = self.entry_chat.get()
 
         widget_message = Message(self.frame_chat, text=f'jesus {message}')
-        widget_message.place()
+        widget_message.grid()
+
+        self.y = self.y + 0.10
 
 
 if __name__ == '__main__':
