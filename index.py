1553fbc29693670f89cfb2a86e8c642d1e791eb9
Jesus <jesus.flores.angulo@gmail.com>
2023-09-12 11:40:53 -0500

    Se cambio todo el codigo del frame chat por un modulo mas eficiente

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from tkinter import ttk
 from tkinter import * 
+from tkscrolledframe import ScrolledFrame
 
 import socket
 
@@ -48,48 +49,31 @@ class App:
 
         """Buttons"""
         #Chat Button
-        self.button_chat = Button(self.label_chat, text='Enviar')
+        self.button_chat = Button(self.label_chat, text='Enviar', command=self.place_mymessage)
         self.button_chat.place(relwidth = 0.10, relheight = 0.05, relx = 0.78, rely = 0.92)
 
         #Image Button
         self.button_sendimg = Button(self.label_chat, text='Imagen')
         self.button_sendimg.place(relwidth = 0.10, relheight = 0.05, relx = 0.89, rely = 0.92)
 
+        """ScrolledFrame"""
+        #Create a ScrolledFrame widget
+        self.frame_helper = ScrolledFrame(self.label_chat)
+        self.frame_helper.place(relwidth = 0.99, relheight = 0.91, relx = 0.0, rely = 0.0)
+        self.frame_helper.bind_scroll_wheel(self.wind)
 
-        """Messages"""
-        self.widget_messages = Message(self.label_chat, text='Prueba')
-        #self.widget_messages.place(relwidth = 0.10, relheight = 0.05, relx = 0.50, rely = 0.50)
-
-
-        """Chat"""
-        #Chat Frame
-        self.frame_chat = Frame(self.label_chat)
-        self.frame_chat.configure(background='#1F1F1F', relief=SOLID, borderwidth=2)
-        self.frame_chat.place(relwidth = 0.9998, relheight = 0.91, relx = 0.0, rely = 0.0)
-
-        #Chat Canvas
-        self.canvas_chat = Canvas(self.frame_chat)
-        self.canvas_chat.configure(background='gray', relief=SOLID, borderwidth=0, highlightbackground='#1F1F1F')
-        self.canvas_chat.place(relwidth = 0.9998, relheight = 0.9999, relx = 0.0, rely = 0.0)
+        #Create a frame within the ScrolledFrame
+        self.frame_chat = self.frame_helper.display_widget(Frame) 
+        self.frame_chat.place(relwidth = 0.99, relheight = 0.91, relx = 0.0, rely = 0.0)
         
-        #Chat Scrollbar
-        self.scrollbar_chat = Scrollbar(self.frame_chat, orient=VERTICAL, command=self.canvas_chat.yview)
-        self.scrollbar_chat.configure()
-        self.scrollbar_chat.place(relwidth = 0.05, relheight = 0.996, relx = 0.92, rely = 0)
-        
-        #Configure Chat Canvas with Chat Scrollbar
-        self.canvas_chat.configure(yscrollcommand=self.scrollbar_chat.set)
-        self.canvas_chat.bind('<Configure>', lambda e: self.canvas_chat.configure(scrollregion=self.canvas_chat.bbox('all'))) 
 
-        #Helper Frame
-        self.frame_helper = Frame(self.canvas_chat)
-        self.canvas_chat.create_window((0,0), window=self.frame_helper, anchor=NW)
 
-        for messages in range(100):
-            Message(self.frame_helper, text=f'Nakuru {messages}').grid()
-            
-    
-        
+    def place_mymessage(self):
+        message = self.entry_chat.get()
+
+        widget_message = Message(self.frame_chat, text=f'jesus {message}')
+        widget_message.place()
+
 
 if __name__ == '__main__':
     WindowT = Tk()
