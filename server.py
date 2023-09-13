8ab7f04ded96c317518e315f99762f6adcecaf59
Jesus <jesus.flores.angulo@gmail.com>
2023-09-13 15:21:50 -0500

    Server creado de forma correcta

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import threading
 
 HEADER = 1024
 PORT = 8080
-SERVER = socket.gethostbyname(socket.gethostname)
+SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
@@ -11,4 +11,32 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+def  handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
 
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            if msg_length.isdigit():
+                msg_length = int(msg_length)
+                msg = conn.recv(msg_length).decode(FORMAT)
+
+                if msg == DISCONNECT_MESSAGE:
+                    connected = False
+
+                print(f"[{addr}] {msg}")
+    
+    conn.close()
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+print("[STARTING] server is starting....")
+start()
