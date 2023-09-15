2436b705839b6cf9ede60fa72ea62f6f44444cd7
Jesus <jesus.flores.angulo@gmail.com>
2023-09-15 12:01:23 -0500

    Se agrego un apartado para seleccionar el nombre de usuario

--- index.py
+++ index.py
@@ -10,98 +10,172 @@ class App:
     def __init__(self, WindowT):
         #Window Attributes
         self.wind = WindowT
-        self.wind.title('Chat')
-        self.wind.geometry('900x700')
+        self.wind.title('Select Username')
+        self.wind.geometry('400x200')
+        #self.wind.geometry('900x700')
         self.wind.configure(bg='#1F1F1F')
 
         #Constants
         self.HEADER = 4064
-        self.PORT = 8902
+        self.PORT = 8009
         self.FORMAT = 'utf-8'
-        self.DISCONNECT_MESSAGE = "!DISCONNECT"
+        self.DISCONNECT_MESSAGE = "!DISCONNECT".encode(self.FORMAT)
         self.SERVER = "192.168.1.205"
         self.ADDR = (self.SERVER, self.PORT)
 
         #Variables
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect(self.ADDR)
-        self.username = 'jesus'
+        self.username = ''
         self.x = 0.80
         self.y = 0
         self.arrived_message = "False"
         self.recieve_length = 4064
+        self.type_connection = ["username", "message"]
        
 
         """Labels"""
         #Username Label
         self.label_username = Label(self.wind, text='one')
         self.label_username.configure(background='#1F1F1F', relief=SOLID, borderwidth=2, fg='gray')
-        self.label_username.place(relwidth = 0.30, relheight = 0.09, relx = 0.0, rely = 0.0)
-
+        #self.label_username.place(relwidth = 0.30, relheight = 0.09, relx = 0.0, rely = 0.0)
+        
         #Contacts Label
         self.label_contacts = Label(self.wind, text='two')
         self.label_contacts.configure(background='#1F1F1F', relief=SOLID, borderwidth=2, fg='gray')
-        self.label_contacts.place(relwidth = 0.30, relheight = 0.91, relx = 0.0, rely = 0.09)
-
-        #Chat Laprint("listo")bel
+        #self.label_contacts.place(relwidth = 0.30, relheight = 0.91, relx = 0.0, rely = 0.09)
+        
+        #Chat Label
         self.label_chat = Label(self.wind)
         self.label_chat.configure(background='#1F1F1F', relief=SOLID, borderwidth=2, fg='gray')
-        self.label_chat.place(relwidth = 0.70, relheight = 0.90, relx = 0.30, rely = 0.0)
-
+        #self.label_chat.place(relwidth = 0.70, relheight = 0.90, relx = 0.30, rely = 0.0)
+        
         #Widget Chat Label
         self.label_wchat = Label(self.wind)
         self.label_wchat.configure(background='#1F1F1F', relief=SOLID, borderwidth=2, fg='gray')
-        self.label_wchat.place(relwidth = 0.70, relheight = 0.10, relx = 0.30, rely = 0.90)
-
+        #self.label_wchat.place(relwidth = 0.70, relheight = 0.10, relx = 0.30, rely = 0.90)
+        
+        #Select Username Label
+        self.label_user = Label(self.wind)
+        self.label_user.configure(background='#1F1F1F', relief=SOLID, borderwidth=2, fg='gray')
+        self.label_user.place(relwidth = 0.9999, relheight = 0.9999, relx = 0.0, rely = 0.0)
 
         """Entrys"""
         #Contacts Chat
         self.entry_contacts = Entry(self.label_contacts, font=('Arial', 15))
         self.entry_contacts.configure(exportselection=False, bg='#323232', fg='white', highlightbackground='gray')
-        self.entry_contacts.place(relwidth = 0.9999, relheight = 0.05, relx = 0.0, rely = 0.0)
-
+        #self.entry_contacts.place(relwidth = 0.9999, relheight = 0.05, relx = 0.0, rely = 0.0)
+        
         #Chat Entry
         self.entry_chat = Entry(self.label_wchat, font=('Arial', 15))
         self.entry_chat.configure(exportselection=False, bg='#323232', fg='white', highlightbackground='gray')
-        self.entry_chat.place(relwidth = 0.75, relheight = 0.65, relx = 0.02, rely = 0.08)
+        #self.entry_chat.place(relwidth = 0.75, relheight = 0.65, relx = 0.02, rely = 0.08)
+        
+        #Select Username Entry
+        self.entry_user = Entry(self.label_user, font=('Arial', 15))
+        self.entry_user.configure(exportselection=False, bg='#323232', fg='white', highlightbackground='gray')
+        self.entry_user.place(relwidth = 0.70, relheight = 0.18, relx = 0.16, rely = 0.25)
+        self.entry_user.insert(END, "       Enter Username")
 
 
         """Buttons"""
         #Chat Button
         self.button_chat = Button(self.label_wchat, text='Enviar', command=self.send_message)
-        self.button_chat.place(relwidth = 0.10, relheight = 0.65, relx = 0.78, rely = 0.08)
-
+        #self.button_chat.place(relwidth = 0.10, relheight = 0.65, relx = 0.78, rely = 0.08)
+        
         #Image Button
         self.button_sendimg = Button(self.label_wchat, text='Imagen')
-        self.button_sendimg.place(relwidth = 0.10, relheight = 0.65, relx = 0.89, rely = 0.08)
+        #self.button_sendimg.place(relwidth = 0.10, relheight = 0.65, relx = 0.89, rely = 0.08)
+        
+        #Accept User Button
+        self.button_user = Button(self.label_user, text='Aceptar')
+        self.button_user.place(relwidth = 0.25, relheight = 0.18, relx = 0.39, rely = 0.55)
 
 
         """Chat"""
-        #ListBox
-        self.listbox_chat = Text(self.label_chat, font=('Arial', 15))
-        self.listbox_chat.configure(exportselection=False, bg='white', fg='gray', highlightbackground='gray')
-        self.listbox_chat.place(relwidth = 0.95, relheight = 0.999, relx = 0.0, rely = 0.0)
-
-        #ScrollBar
-        self.scrollbar_chat = Scrollbar(self.label_chat, command=self.listbox_chat.yview)
-        self.listbox_chat.configure(yscrollcommand=self.scrollbar_chat.set)
-        self.scrollbar_chat.configure(background='#444444', activebackground='gray')
-        self.scrollbar_chat.place(relwidth = 0.05, relheight = 0.999, relx = 0.95, rely = 0)
+        #Textbox
+        self.textbox_chat = Text(self.label_chat, font=('Arial', 15))
+        self.textbox_chat.configure(exportselection=False, bg='white', fg='gray', highlightbackground='gray')
+        #self.textbox_chat.place(relwidth = 0.95, relheight = 0.999, relx = 0.0, rely = 0.0)
         
+        #ScrollBar
+        self.scrollbar_chat = Scrollbar(self.label_chat, command=self.textbox_chat.yview)
+        self.textbox_chat.configure(yscrollcommand=self.scrollbar_chat.set)
+        self.scrollbar_chat.configure(background='#444444', activebackground='gray')
+        #self.scrollbar_chat.place(relwidth = 0.05, relheight = 0.999, relx = 0.95, rely = 0)
+        
+        #Threads
         self.request_thread = threading.Thread(target=self.recieve_message)
         self.request_thread.start()
-
         self.response_thread = threading.Thread(target=self.send_message)
+        
+        self.Eventks()
+
+    def Eventks(self):
+        self.wind.protocol("WM_DELETE_WINDOW", self.closing_window)
+
+    #Function to manage the closing window
+    def closing_window(self):
+        #Disconnect message length
+        send_length = self.length_message(self.DISCONNECT_MESSAGE)
+
+        self.recieve_message(connected=True)
+
+        #Sending the message and the length
+        self.client.send(send_length)
+        self.client.send(self.DISCONNECT_MESSAGE)
+        
+        self.wind.destroy()
+
+    #Function to select username 
+    def select_username(self):
+        #Get the username
+        self.username = self.entry_user.get().encode(self.FORMAT)
+
+        #Type connection
+        send_length = self.length_message(self.type_connection[0])#Username
+        self.client.send(send_length)
+        self.client.send(self.type_connection[0])#Username
+        self.recieve_length = send_length
+
+        #Extract the length and encode the message
+        send_length = self.length_message(self.username)
+        self.recieve_length = send_length
+
+        #Sending the message and the length
+        self.client.send(send_length)
+        self.client.send(self.username)
+
+        self.chat_stage()
+
+    def chat_stage(self):
+        self.entry_chat.place_forget()
+        self.button_user.place_forget()
+        self.label_user.place_forget()
+
+        self.wind.geometry("900x700")
+        self.wind.title("Chat")
+        
+        self.label_username.place(relwidth = 0.30, relheight = 0.09, relx = 0.0, rely = 0.0)
+        self.label_contacts.place(relwidth = 0.30, relheight = 0.91, relx = 0.0, rely = 0.09)
+        self.label_chat.place(relwidth = 0.70, relheight = 0.90, relx = 0.30, rely = 0.0)
+        self.label_wchat.place(relwidth = 0.70, relheight = 0.10, relx = 0.30, rely = 0.90)
+        self.entry_contacts.place(relwidth = 0.9999, relheight = 0.05, relx = 0.0, rely = 0.0)
+        self.entry_chat.place(relwidth = 0.75, relheight = 0.65, relx = 0.02, rely = 0.08)
+        self.button_chat.place(relwidth = 0.10, relheight = 0.65, relx = 0.78, rely = 0.08)
+        self.button_sendimg.place(relwidth = 0.10, relheight = 0.65, relx = 0.89, rely = 0.08)
+        self.textbox_chat.place(relwidth = 0.95, relheight = 0.999, relx = 0.0, rely = 0.0)
+        self.scrollbar_chat.place(relwidth = 0.05, relheight = 0.999, relx = 0.95, rely = 0)
+        
 
     #Function to send the message and username
     def send_message(self):
         #Get the message
-        msg = self.entry_chat.get()
-        msg = msg.encode(self.FORMAT)
+        message = self.entry_chat.get()
+        message = message.encode(self.FORMAT)
 
         #Extract the length and encode the message
-        send_length = self.length_message(msg)[0]
-        message = self.length_message(msg)[1]
+        send_length = self.length_message(message)
         self.recieve_length = send_length
 
         #Sending the message and the length
@@ -109,25 +183,23 @@ class App:
         self.client.send(message)
 
     #Recieve message
-    def recieve_message(self):  
-        while True:
+    def recieve_message(self, connected = None):  
+        while connected == None:
             try:
                 self.recieve_length = int(self.recieve_length)
                 msg = self.client.recv(self.recieve_length * 90).decode(self.FORMAT)
-                self.listbox_chat.insert(END, f"jesus : {msg} \n")
+                self.textbox_chat.insert(END, f"{self.username} : {msg} \n")
                 
             except Exception as ex:
-                print("An error ocurred")
-                self.client.close()
-                print(ex)
-                break
+                print("Hay ocurrido este error", ex)
+            break
          
     #Function to extract the length
     def length_message(self, msg):
         msg_length = len(msg)
         send_length = str(msg_length).encode(self.FORMAT)
         send_length += b' ' * (self.HEADER - len(send_length))
-        return [send_length, msg]
+        return send_length
 
 if __name__ == '__main__':
     WindowT = Tk()
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 HEADER = 4064
-PORT = 8902
+PORT = 8009
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
@@ -27,32 +27,37 @@ def messages_clients(client):
             msg_length = client.recv(HEADER).decode(FORMAT)
             msg_length = int(msg_length)
             msg = client.recv(msg_length).decode(FORMAT)
-            broadcast_clients(msg, client)
+            if msg == DISCONNECT_MESSAGE:
+                broadcast_clients(f"Se desconecto Jesus \n", client)
+                clients.remove(client)
+                client.close()
+            else:
+                broadcast_clients(msg, client)
 
         except:
-            broadcast_clients(f"Se desconecto Jesus \n", client)
-            clients.remove(client)
-            client.close()
-            break
-            
+            break 
 
-def start():
+def handle_clients(conn, addr):
     global clients
-    print(f"[LISTENING] Server is listening on {SERVER}")
-    while True:
-        conn, addr = server.accept()
-       
-        clients.append(conn)
 
+    while True:
+        clients.append(conn)
         wel_message = "Jesus se unio al chat \n"
         broadcast_clients(wel_message, conn)
         conn.send("Conectado al server \n".encode(FORMAT))
 
-
         messages_thread = threading.Thread(target=messages_clients, args=(conn,))
         messages_thread.start()
 
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+        
 
+
+def start():
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    conn, addr = server.accept()
+    handle_thread = threading.Thread(target=handle_clients, args=(conn, addr))
+    handle_thread.start()
+    print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+    
 print("[STARTING] server is starting....")
 start()
